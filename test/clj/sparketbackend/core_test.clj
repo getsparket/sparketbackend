81c03a7f656080596816715d8da7a0f1615e2f58
Matthew Marks <matthewfmarks@gmail.com>
2017-12-19 16:18:20 -0500

    beginning of integration test for complete state flow

--- test/clj/sparketbackend/core_test.clj
+++ test/clj/sparketbackend/core_test.clj
@@ -91,3 +91,24 @@
         (do
           (Thread/sleep 5500) ;; HACK 5500 guarantees http-loop runs at least once.
           (is (= after-handle-start @twil/customer-accounts))))))))
+(deftest customer-gets-thing
+  (let [dummy-phone-number (:dummy-phone-number env)
+        another-dummy      (:twilio-dev-phone-number env)] ;; FIXME change!
+    (with-redefs [twil/customer-accounts (atom {})
+                  twil/dispatched-messages (atom #{})
+                  ;; normal repl flow: in test-mode, do not mount/start the whole app. do not start http-loop.
+                  ]
+      (let [start-txt [{:from dummy-phone-number
+                         :body "Hello!"
+                         :to another-dummy}]
+            after-handle-start {dummy-phone-number
+                                #:cust{:state 'Ready,
+                                       :txts
+                                       [{:from dummy-phone-number
+                                         :body "Hello!"
+                                         :to another-dummy}]}}]
+        (testing "intending to test that a customer can go from start to ready to identifying item"
+          (do
+            (twil/put!-new-messages start-txt)
+            (Thread/sleep 100) ;; HACK async/put!, then later take! then later dispatch-new-messages takes a little time. TODO put these in a parking loop
+            (is (= after-handle-start @twil/customer-accounts))))))))
